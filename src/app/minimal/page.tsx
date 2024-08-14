a52433069f895ff3a467d3da706103ea50403d73
imBanden <brandon@majilabs.com>
2024-08-14 19:45:07 +0100

    added footer to balance

--- src/app/minimal/page.tsx
+++ src/app/minimal/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import IconBadge from "@/components/IconBadge";
+import TertiaryButton from "@/components/TertiaryButton";
+import LogosInstagramIcon from "@/icons/LogosInstagramIcon";
+import LogosLinkedinIcon from "@/icons/LogosLinkedinIcon";
+import LogosSpotifyIcon from "@/icons/LogosSpotifyIcon";
+import LogosTwitter from "@/icons/LogosTwitter";
+import LogosYoutubeIcon from "@/icons/LogosYoutubeIcon";
 import React from "react";
 
 const page = () => {
@@ -30,7 +39,7 @@ const page = () => {
       {/* nav bar */}
       <div className="bg-zinc-50 flex justify-between items-center px-6 py-6 sticky top-0">
         <div className="font-satoshi font-bold text-3xl">balance.</div>
-        <button className="text-lg py-3 px-6 text-zinc-100 rounded-xl font-bold transition-all bg-gradient-to-t from-zinc-700 to-zinc-900 hover:from-zinc-950 hover:to-zinc-950 shadow-[0_10px_15px_rgba(0,0,0,0.04),0_2px_4px_rgba(0,0,0,0.15)]">
+        <button className="text-lg py-3 px-6 text-zinc-100 rounded-xl font-bold transition-all bg-gradient-to-t from-zinc-700 to-zinc-900 hover:from-zinc-950 hover:to-zinc-950 shadow-[2px_4px_12px_rgba(0,0,0,0.08)]">
           Buy now
         </button>
       </div>
@@ -49,11 +58,11 @@ const page = () => {
       <div className="bg-zinc-950 h-[90vh] flex justify-center py-6">
         <div className="mx-24 w-full h-full flex flex-col items-center">
           <div className="flex w-full">
-            <div className="flex flex-1 flex-col text-zinc-50 gap-6">
+            <div className="flex flex-2 flex-col text-zinc-50 gap-6 px-8">
               <p className="font-satoshi font-bold text-5xl">
                 We love to connect with you
               </p>
-              <p>
+              <p className="opacity-80">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos in
                 ducimus aspernatur minima, labore voluptatem molestias
                 praesentium voluptate veritatis perspiciatis illo totam tempore
@@ -62,19 +71,44 @@ const page = () => {
                 temporibus esse dolores odio suscipit animi perferendis
                 accusamus dolorum? Voluptatum.
               </p>
-              <button className="text-lg py-3 px-6 bg-none rounded-xl border-2 border-zinc-50 text-zinc-200 font-bold hover:bg-zinc-800 hover:text-zinc-100 transition-all cursor-pointer">
-                Lets connect
+              <button className="w-fit text-lg py-3 px-6 bg-none rounded-xl border-2 border-zinc-50 text-zinc-200 font-bold hover:bg-zinc-800 hover:text-zinc-100 transition-all cursor-pointer">
+                Let's connect
               </button>
             </div>
-            <div className="flex flex-1  flex-col text-zinc-50">
-              <p className="font-satoshi font-bold ">
-                We love to connect with you
-              </p>
+            <div className="flex flex-1  flex-col text-zinc-50 px-8 gap-6">
+              <p className="font-satoshi font-bold text-5xl">Socials</p>
+              <div className="flex flex-col gap-3 w-fit">
+                <IconBadge>
+                  <LogosYoutubeIcon className="text-2xl" />
+                </IconBadge>
+                <IconBadge>
+                  <LogosInstagramIcon className="text-3xl" />
+                </IconBadge>
+                <IconBadge>
+                  <LogosSpotifyIcon className="text-3xl" />
+                </IconBadge>
+                <IconBadge>
+                  <LogosLinkedinIcon className="text-3xl" />
+                </IconBadge>
+                <IconBadge>
+                  <LogosTwitter className="text-2xl" />
+                </IconBadge>
+              </div>
+            </div>
+
+            <div className="flex flex-1  flex-col text-zinc-50 px-8 gap-6">
+              <p className="font-satoshi font-bold text-5xl">Others</p>
+              <div className="flex flex-col gap-3 w-fit">
+                <TertiaryButton>Diem.</TertiaryButton>
+                <TertiaryButton>NeonCore</TertiaryButton>
+                <TertiaryButton>CyberPulse</TertiaryButton>
+                <TertiaryButton>ForrestyUI</TertiaryButton>
+              </div>
             </div>
           </div>
-          <h1 className="font-satoshi text-zinc-50 text-9xl font-bold text-[20em]">
+          <div className="font-satoshi text-zinc-50 text-9xl font-bold text-[20em] pointer-events-none">
             balance.
-          </h1>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/TertiaryButton.tsx
@@ -0,0 +1,26 @@
+import React, { Children, useState } from "react";
+
+interface TertiaryButtonProps {
+  children: React.ReactNode;
+}
+
+const TertiaryButton = ({ children }: TertiaryButtonProps) => {
+  const [isHovering, setIsHovering] = useState<boolean>(false);
+
+  return (
+    <div
+      className="flex flex-col font-satoshi font-bold text-zinc-50 cursor-pointer w-fit"
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <p className="select-none">{children}</p>
+      <div
+        className={`bg-zinc-50 transition-all duration-500 ${
+          isHovering ? "w-full" : "w-0"
+        } h-[2px]`}
+      />
+    </div>
+  );
+};
+
+export default TertiaryButton;
